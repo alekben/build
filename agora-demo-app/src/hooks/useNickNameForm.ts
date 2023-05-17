1a7e84fce355c95fb7e19f653db6521ca70cc9fd
alekben <abenditch@gmail.com>
2023-05-17 14:03:39 -0700

    fcr

---
+++ agora-demo-app/src/hooks/useNickNameForm.ts
@@ -0,0 +1,20 @@
+import { useI18n } from 'agora-common-libs';
+import { useMemo } from 'react';
+
+export const useNickNameForm = () => {
+  const transI18n = useI18n();
+  const rule = useMemo(
+    () => [
+      {
+        required: true,
+        message: transI18n('fcr_join_room_tips_name'),
+      },
+      {
+        pattern: /^([a-zA-Z0-9_\u4e00-\u9fa5]{0,50})$/,
+        message: transI18n('fcr_create_room_tips_name_rule'),
+      },
+    ],
+    [transI18n],
+  );
+  return { rule };
+};
